6b96200b8de0d00a0b375e563881de592caf8688
Leif Denby <leif@denby.eu>
2021-07-30 12:49:50 +0200

    remove unused code

--- cloudmetrics_pipeline/process.py
+++ cloudmetrics_pipeline/process.py
@@ -1,6 +1,5 @@
 import yaml
 import xarray as xr
-from tqdm import tqdm
 from pathlib import Path
 import luigi
 import inspect
@@ -54,18 +53,6 @@ def _compute_metrics_on_cloudmask(da_cloudmask, metrics):
         metric_value = fn_metric(cloud_mask=da_cloudmask.values)
         ds_metrics[metric] = metric_value
     return ds_metrics
-
-
-def main(data_path, metrics=["fractal_dimension"]):
-    scene_ids = _load_scene_ids(data_path=data_path)
-    for scene_id, source_filepath in tqdm(scene_ids.items()):
-        da_cloudmask = xr.open_dataarray(source_filepath)
-        ds_metrics = _compute_metrics_on_cloudmask(
-            da_cloudmask=da_cloudmask, metrics=metrics
-        )
-        ds_metrics["scene_id"] = scene_id
-        filepath_scene_metrics = data_path / SCENE_PATH / f"{scene_id}.metrics.nc"
-        ds_metrics.to_netcdf(filepath_scene_metrics)
 
 
 class SourceFile(luigi.Task):
@@ -245,14 +232,3 @@ def find_scenes(source_files):
     filename.
     """
     return CloudmetricPipeline(source_files=source_files)
-
-
-if __name__ == "__main__":
-    import argparse
-
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("--data-path", default=".", type=Path)
-    argparser.add_argument("--metrics", default=["fractal_dimension"], nargs="+")
-    args = argparser.parse_args()
-
-    main(**vars(args))
