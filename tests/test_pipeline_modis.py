661fa1867dd3768701377b1f02b1be486a98511c
Leif Denby <leif@denby.eu>
2021-07-30 16:45:24 +0200

    fix modis rgb test

--- tests/test_pipeline_modis.py
+++ tests/test_pipeline_modis.py
@@ -8,18 +8,13 @@ def test_MODIS_greyscale_metrics():
     data_path = Path("/tmp/cm_pipeline")
     data_path.mkdir(exist_ok=True, parents=True)
 
-    cm_pipeline.download.modis.downloadMODISImgs(
-        startDate="2002-12-01",
-        endDate="2002-12-02",
-        extent=[-58, -48, 10, 20],
-        savePath=str(data_path),
-        exist_skip=True,
-    )
-
-    filepaths = list(data_path.glob("*.jpeg"))
-
     (
-        cm_pipeline.find_scenes(source_files=filepaths)
+        cm_pipeline.download.modis_rgb(
+            start_date="2020-01-01",
+            end_date="2020-01-02",
+            bbox=[-58, -48, 10, 20],
+            data_path=data_path,
+        )
         .mask(fn=cm_pipeline.masks.rgb_greyscale_mask)
         .compute_metrics(metrics=["fractal_dimension"])
         .execute()
